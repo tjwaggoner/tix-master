84d486dccbd89ebe4dfeee04b3cf430bc33315ee
Tanner Waggoner <tanner.waggoner+data@databricks.com>
2025-12-18 09:07:36 -0500

    Fix stored procedures to execute after creation for ETL logging

--- sql/stored_procedures/sp_generate_event_summary.sql
+++ sql/stored_procedures/sp_generate_event_summary.sql
@@ -201,3 +201,17 @@ BEGIN
   );
 
 END;
+
+-- Execute the procedure
+DECLARE total_reports INT;
+DECLARE execution_status STRING;
+
+CALL ticket_master.gold.sp_generate_event_summary(
+  2025,
+  total_reports,
+  execution_status
+);
+
+SELECT 
+  total_reports,
+  execution_status;
