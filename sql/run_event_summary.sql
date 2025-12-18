e07263383603d3faa00120ddbed5649287801e70
Tanner Waggoner <tanner.waggoner+data@databricks.com>
2025-12-18 09:11:42 -0500

    Separate stored procedure creation from execution in ETL pipeline

---
+++ sql/run_event_summary.sql
@@ -0,0 +1,15 @@
+-- Run Event Summary Generation Procedure
+
+DECLARE total_reports INT;
+DECLARE execution_status STRING;
+
+CALL ticket_master.gold.sp_generate_event_summary(
+  2025,
+  total_reports,
+  execution_status
+);
+
+SELECT 
+  total_reports,
+  execution_status;
+
